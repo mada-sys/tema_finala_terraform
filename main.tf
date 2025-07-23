211fb5b23fe193783cee1a2f670d7ef54b57e82f
mada-sys <madalina.vasile@student.unitbv.ro>
2025-07-23 21:56:19 +0300

    e

--- main.tf
+++ main.tf
@@ -9,34 +9,8 @@ variable "vm_size" {
 variable "vm_image" {
   default = "22_04-lts"
 }
-variable "admin_password" {
-  description = "Parola pentru utilizatorul admin"
-  type        = string
-  sensitive   = true
-}
-variable "client_id" {
-  description = "Azure client ID"
-  type        = string
-  sensitive   = true
-}
 
-variable "client_secret" {
-  description = "Azure client secret"
-  type        = string
-  sensitive   = true
-}
 
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-  sensitive   = true
-}
 
 locals {
   prefix         = "mada"
